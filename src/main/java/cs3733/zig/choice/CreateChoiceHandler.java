adb77530809361567542af738fcb260e3d761d34
timmy <timmy@130.215.12.139>
2020-11-19 16:40:46 -0500

    Added lambda function for register for choice, it only checks whether the choice ID is in the db. Created a connection to db (db util) and created a choices DAO

--- src/main/java/cs3733/zig/choice/CreateChoiceHandler.java
+++ src/main/java/cs3733/zig/choice/CreateChoiceHandler.java
@@ -1,17 +1,12 @@
 package cs3733.zig.choice;
 
 import java.util.List;
+
 import java.util.UUID;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-
-import cs3733.zig.choice.db.ChoicesDAO;
-import cs3733.zig.choice.http.CreateChoiceRequest;
-import cs3733.zig.choice.http.CreateChoiceResponse;
-import cs3733.zig.choice.model.Alternative;
-import cs3733.zig.choice.model.Choice;
 
 public class CreateChoiceHandler implements RequestHandler<Object, String> {
 	
