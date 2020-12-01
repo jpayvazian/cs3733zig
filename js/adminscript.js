13c8552537b2e5c122d3b1c7d9db319bd4fdf548
timmy <timmy@130.215.12.139>
2020-12-01 12:21:46 -0500

    Got rid of innerHTML

--- js/adminscript.js
+++ js/adminscript.js
@@ -14,14 +14,25 @@ window.onload = () => {
 			const choices = json.choices
 			let output = ""
 			if(json.statusCode==200){
+				$( "#listOfChoicesReport" ).empty();
 				for(let i = 0; i<choices.length; i++){
 					var date = new Date (choices[i].startDate);
-					output = output + "<h3>" + choices[i].id + ": " + "</h3><br>"
-					output = output + "DATE OF CREATION: " + date + ", " + "COMPLETED: " + choices[i].completed + "<hr>"
-					output = "<div class=\"choiceElement\">" + output +  "</div>"
+					
+					const choiceElement = document.createElement('div')
+					choiceElement.setAttribute('class','choiceElement')
+					const h3 = document.createElement('h3')
+					h3.innerText = choices[i].id + ": " + "\n"
+					const info = document.createElement('p')
+					info.innerText = "DATE OF CREATION: " + date + ", " + "COMPLETED: " + choices[i].completed
+					const horizontal = document.createElement('hr')
+					choiceElement.append(h3)
+					choiceElement.append(info)
+					choiceElement.append(horizontal)
+					
+					reportedChoices.append(choiceElement)
 				}
 				
-				reportedChoices.innerHTML = output
+				
 			}
 			else alert("Error!")
         })
