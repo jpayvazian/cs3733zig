0352c262d10b8ea4b7afc5b5061ad175e21552bc
theratzoo <lderatzou@wpi.edu>
2020-12-09 22:40:07 -0500

    stuff I forgot to pusah

--- js/choicescript.js
+++ js/choicescript.js
@@ -29,7 +29,10 @@ window.onload = () => {
 				number += 1
 			}
 			createCarousel(aNames, aDescrips, aIds, aApproves, aDisapproves, number, aFeedbacks)
-			document.querySelector('#code').innerText = json.choice.id
+            document.querySelector('#code').innerText = json.choice.id
+            if(json.choice.completed) {
+                completeChoice(json.choice.chosenAlternative.name)
+            }
 		} else {
 			console.log("ERROR")
 		}		
@@ -37,244 +40,179 @@ window.onload = () => {
 }
 
 function changeRating(self, strNum, otherInt) {
-        const strs = ["up", "down"]
-		const memberName = localStorage.getItem('memberName')
-		const rating = self.childNodes[0].src.includes('up')
-		const idAlternative = document.querySelectorAll('.idAlternative')[otherInt].innerText
-		fetch(url+"/addRating", {
-			method:'POST',
-			body:JSON.stringify({rating, memberName, idAlternative})
-		})
-		.then(response=>response.json())
-		.then(json=>{
-			console.log(json)
-			if(json.statusCode==200) {
-				//below only happens if successful
-		        if(self.childNodes[0].src.includes('blue')) {
-		            self.childNodes[0].src=`/img/${strs[strNum]}.png`
-		        } else {
-		            self.childNodes[0].src=`/img/blue${strs[strNum]}.png`
-		        }
-		        document.querySelectorAll(`.${strs[Math.abs(strNum-1)]}-rating`)[otherInt].src=`/img/${strs[Math.abs(strNum-1)]}.png`
-				document.querySelectorAll('.upcount')[otherInt].innerText=json.approvers.length
-				document.querySelectorAll('.downcount')[otherInt].innerText=json.disapprovers.length
-				const aNames = document.querySelectorAll('.approveul')[otherInt]
-				while (aNames.firstChild) {
-				    aNames.removeChild(aNames.lastChild)
-				  }
-				for(let i=0; i<json.approvers.length; i++) {
-					const li = document.createElement('li')
-					li.innerText=json.approvers[i]
-					aNames.append(li)
-				}
-				const dNames = document.querySelectorAll('.disapproveul')[otherInt]
-				while (dNames.firstChild) {
-				    dNames.removeChild(dNames.lastChild)
-				  }
-				for(let i=0; i<json.disapprovers.length; i++) {
-					const li = document.createElement('li')
-					li.innerText=json.disapprovers[i]
-					dNames.append(li)
-				}
-		        //in here, also make sure that it deselects the other one!
-			} else {
-				console.log("ERROR!")
-			}
-		})
-
-		
+    const strs = ["up", "down"]
+    let src = null;
+    for(let i=0; i<self.childNodes.length; i++) {
+        if(self.childNodes[i].src!=null) src=self.childNodes[i]
     }
+    const memberName = localStorage.getItem('memberName')
+    const rating = src.src.includes('up')
+    const idAlternative = document.querySelectorAll('.idAlt')[otherInt].innerText
+    fetch(url+"/addRating", {
+        method:'POST',
+        body:JSON.stringify({rating, memberName, idAlternative})
+    })
+    .then(response=>response.json())
+    .then(json=>{
+        console.log(json)
+        if(json.statusCode==200) {
+            //below only happens if successful
+            if(src.src.includes('blue')) {
+                src.src=`/img/${strs[strNum]}.png`
+            } else {
+                src.src=`/img/blue${strs[strNum]}.png`
+            }
+            document.querySelectorAll(`.${strs[Math.abs(strNum-1)]}-rating`)[otherInt].src=`/img/${strs[Math.abs(strNum-1)]}.png`
+            document.querySelectorAll('.upcount')[otherInt].innerText=json.approvers.length
+            document.querySelectorAll('.downcount')[otherInt].innerText=json.disapprovers.length
+            const aNames = document.querySelectorAll('.approveul')[otherInt]
+            while (aNames.firstChild) {
+                aNames.removeChild(aNames.lastChild)
+                }
+            for(let i=0; i<json.approvers.length; i++) {
+                const li = document.createElement('li')
+                li.innerText=json.approvers[i]
+                aNames.append(li)
+            }
+            const dNames = document.querySelectorAll('.disapproveul')[otherInt]
+            while (dNames.firstChild) {
+                dNames.removeChild(dNames.lastChild)
+            }
+            for(let i=0; i<json.disapprovers.length; i++) {
+                const li = document.createElement('li')
+                li.innerText=json.disapprovers[i]
+                dNames.append(li)
+            }
+            //in here, also make sure that it deselects the other one!
+        } else {
+            console.log("ERROR!")
+        }
+    })	
+}
 
 function showFeedbackAddForm(altNum) {
-    	const theDiv = document.querySelectorAll('.feedbackForm')[altNum]
-    	if(theDiv.childNodes.length==0) {
-    		const field = document.createElement('textarea')
-    		field.setAttribute('id', `feedbackTextArea${altNum}`)
-	    	const submitButton = document.createElement('button')
-	    	submitButton.setAttribute('class', 'btn')
-	    	submitButton.setAttribute('class', 'btn-primary')
-	    	submitButton.innerText = 'Submit'
-	    	submitButton.setAttribute('onclick', `addFeedback(${altNum})`)
-	    	theDiv.append(field)
-	    	theDiv.append(submitButton)
-    	}
+    const theDiv = document.querySelectorAll('.feedbackForm')[altNum]
+    if(theDiv.childNodes.length==0) {
+        const field = document.createElement('textarea')
+        field.setAttribute('id', `feedbackTextArea${altNum}`)
+        const submitButton = document.createElement('button')
+        submitButton.setAttribute('class', 'btn')
+        submitButton.setAttribute('class', 'btn-primary')
+        submitButton.innerText = 'Submit'
+        submitButton.setAttribute('onclick', `addFeedback(${altNum})`)
+        theDiv.append(field)
+        theDiv.append(submitButton)
     }
-
-    function addFeedback(altNum) {
-    	const feedback = document.querySelector(`#feedbackTextArea${altNum}`).value
-    	if(feedback=='') {
-    		alert('ERROR: empty text field!')
-    	} else {
-			const idAlternative = document.querySelectorAll('.idAlternative')[altNum].innerText
-			fetch(url+'/addFeedback', {
-				method:'POST',
-				body:JSON.stringify({memberName:localStorage.getItem('memberName'), contents:feedback, idAlternative})
-			})
-			.then(response=>response.json())
-			.then(json=>{
-				console.log(json);
-				if(json.statusCode==200) {
-					console.log(json.feedback)
-					const feedbackP = document.createElement('p')
-					feedbackP.innerText = json.feedback.contents
-					const feedbackMN = document.createElement('h4')
-	            	feedbackMN.innerText = 'By ' + json.feedback.memberName
-	            	feedbackMN.style.marginRight='40px'
-	            	feedbackMN.style.fontStyle = 'italic'
-					const hr = document.createElement('hr')
-					document.querySelector('#feedbackcollapse'+altNum).append(feedbackP)
-					document.querySelector('#feedbackcollapse'+altNum).append(feedbackMN)
-					document.querySelector('#feedbackcollapse'+altNum).append(hr)
-				} else {
-					console.log("ERROR ADDING FEEDBACK!")
-				}
-			})
-    	}
 }
-    	//TODO: make the fetch for addign zee feedback!
+
+function pickAlternative(altNum) {
+    const idAlternative = document.querySelectorAll('.idAlt')[altNum].innerText
+    const idChoice = document.querySelector('#code').innerText
+    fetch(url+'/completeChoice', {
+        method:'POST',
+        body:JSON.stringify({idAlternative, idChoice})
+    })
+    .then(response=>response.json())
+    .then(json=>{
+        if(json.statusCode==200) {
+            alert("Choice is completed! You may no longer modify this choice.")
+            completeChoice(document.querySelectorAll('.altName')[altNum].innerText)
+        } else {
+            console.log("ERROR COMPLETING CHOICE")
+        }
+    })   
+    console.log('YOU HAVE PICKED ALTERNATIVE ' + altNum)
+
+}
+
+function completeChoice(alt) {
+    const end = document.createElement('h2')
+    end.innerText="CHOICE IS COMPLETED."
+    const chosenAlt = document.createElement('h3')
+    chosenAlt.innerText = alt
+    document.querySelector('.jumbotron').append(end)
+    document.querySelector('.jumbotron').append(chosenAlt)
+    const disableAs = document.querySelectorAll('a')
+    Array.prototype.slice.call(document.querySelectorAll("a")).map(i=>i.onclick='')
+    Array.prototype.slice.call(document.querySelectorAll(".onlyForActive")).map(i=>i.remove())
+}
+
+function addFeedback(altNum) {
+    const feedback = document.querySelector(`#feedbackTextArea${altNum}`).value
+    if(feedback=='') {
+        alert('ERROR: empty text field!')
+    } else {
+        const idAlternative = document.querySelectorAll('.idAlt')[altNum].innerText
+        fetch(url+'/addFeedback', {
+            method:'POST',
+            body:JSON.stringify({memberName:localStorage.getItem('memberName'), contents:feedback, idAlternative})
+        })
+        .then(response=>response.json())
+        .then(json=>{
+            console.log(json);
+            if(json.statusCode==200) {
+                console.log(json.feedback)
+                const feedbackP = document.createElement('p')
+                feedbackP.innerText = json.feedback.contents
+                const feedbackMN = document.createElement('h4')
+                feedbackMN.innerText = 'By ' + json.feedback.memberName
+                feedbackMN.style.marginRight='40px'
+                feedbackMN.style.fontStyle = 'italic'
+                const hr = document.createElement('hr')
+                document.querySelector('#feedbackcollapse'+altNum).append(feedbackP)
+                document.querySelector('#feedbackcollapse'+altNum).append(feedbackMN)
+                document.querySelector('#feedbackcollapse'+altNum).append(hr)
+                alert('Feedback added!')
+                const f = document.querySelectorAll('.feedbackForm')[altNum]
+                while (f.firstChild) {
+                    f.removeChild(f.lastChild)
+                }
+            } else {
+                console.log("ERROR ADDING FEEDBACK!")
+            }
+        })
+    }
+}
 
 function createCarousel(aNames, aDescrips, aIds, aApproves, aDisapproves, num, aFeedbacks) {
     const cindicators = document.querySelectorAll('.cli')
     const cinners = document.querySelectorAll('.carousel-item')
     for(let i=0; i<num; i++) {
-            const h1 = document.createElement('h1')
-            h1.innerText=aNames[i]
-            const h4 = document.createElement('h4')
-            h4.innerText=aDescrips[i]            
-			const h6 = document.createElement('h6')
-			h6.setAttribute('class', 'idAlternative')
-			h6.innerText=aIds[i]
-            const approval = document.createElement('img')
-			if(aApproves[i].includes(localStorage.getItem('memberName'))) {
-				approval.setAttribute('src', '/img/blueup.png')
-			} else {
-				approval.setAttribute('src', '/img/up.png')
-			}
+            document.querySelectorAll('.altName')[i].innerText=aNames[i]
+            document.querySelectorAll('.altDes')[i].innerText=aDescrips[i]            
+			document.querySelectorAll('.idAlt')[i].innerText=aIds[i]
             
-            approval.setAttribute('class', 'ratingImg up-rating')
-            const disapproval = document.createElement('img')
-            if(aDisapproves[i].includes(localStorage.getItem('memberName'))) {
-				disapproval.setAttribute('src', '/img/bluedown.png')
+			if(aApproves[i].includes(localStorage.getItem('memberName'))) {
+				document.querySelectorAll('.up-rating')[i].setAttribute('src', '/img/blueup.png')
 			} else {
-				disapproval.setAttribute('src', '/img/down.png')
+				document.querySelectorAll('.up-rating')[i].setAttribute('src', '/img/up.png')
 			}
-            disapproval.setAttribute('class', 'ratingImg down-rating')
-            const approvalA = document.createElement('a')
-            approvalA.setAttribute('onclick', 'changeRating(this, 0, ' + i + ')')
-            approvalA.append(approval)
-            const disapprovalA = document.createElement('a')
-            disapprovalA.setAttribute('onclick', 'changeRating(this, 1, ' + i +')')
-            disapprovalA.append(disapproval)
-            const approvalcount = document.createElement('h5')
-            approvalcount.innerText = aApproves[i].length
-			approvalcount.setAttribute('class', 'upcount')
-            const disapprovalcount = document.createElement('h5')
-            disapprovalcount.innerText = aDisapproves[i].length
-			disapprovalcount.setAttribute('class', 'downcount')
-           const approvalMemberList = document.createElement('div') //THIS WILL BE A COLLAPSABLE ELEMENT
-            const approveButton = document.createElement('button')
-            approveButton.setAttribute('class', 'btn')
-            approveButton.setAttribute('class', 'btn-primary')
-            approveButton.setAttribute('type', 'button')
-            approveButton.setAttribute('data-toggle', 'collapse')
-            approveButton.setAttribute('data-target', `#approvecollapse${i}`)
-            approveButton.innerText="List Of Approved Peeps"
-            approvalMemberList.append(approveButton)
-            const approveDiv = document.createElement('div')
-            approveDiv.setAttribute('class', 'collapse')
-            approveDiv.setAttribute('id', `approvecollapse${i}`)
-            const ul = document.createElement('ul')
-			ul.setAttribute('class', 'approveul')
+            if(aDisapproves[i].includes(localStorage.getItem('memberName'))) {
+				document.querySelectorAll('.down-rating')[i].setAttribute('src', '/img/bluedown.png')
+			} else {
+				document.querySelectorAll('.down-rating')[i].setAttribute('src', '/img/down.png')
+			}
+            document.querySelectorAll('.upcount')[i].innerText = aApproves[i].length
+            document.querySelectorAll('.downcount')[i].innerText = aDisapproves[i].length
+            //list of upvoted users
+            const aul = document.querySelectorAll('.approveul')[i]
             for(let j=0; j<aApproves[i].length; j++) {
             	const li = document.createElement('li')
             	li.innerText=aApproves[i][j]
-            	ul.append(li)
+            	aul.append(li)
             }
-            approveDiv.append(ul)
-            approvalMemberList.append(approveDiv)
-            const disapprovalMemberList = document.createElement('div') //THIS WILL BE A COLLAPSABLE ELEMENT
-            const disapproveButton = document.createElement('button')
-            disapproveButton.setAttribute('class', 'btn')
-            disapproveButton.setAttribute('class', 'btn-primary')
-            disapproveButton.setAttribute('type', 'button')
-            disapproveButton.setAttribute('data-toggle', 'collapse')
-            disapproveButton.setAttribute('data-target', `#disapprovecollapse${i}`)
-            disapproveButton.innerText="List Of Disapproved Peeps"
-            disapprovalMemberList.append(disapproveButton)
-			const disapproveDiv = document.createElement('div')
-            disapproveDiv.setAttribute('class', 'collapse')
-            disapproveDiv.setAttribute('id', `disapprovecollapse${i}`)
-            const disul = document.createElement('ul')
-			disul.setAttribute('class', 'disapproveul')
+            //list of downvoted users
+            const disul = document.querySelectorAll('.disapproveul')[i]
             for(let j=0; j<aDisapproves[i].length; j++) {
             	const li = document.createElement('li')
             	li.innerText=aDisapproves[i][j]
             	disul.append(li)
             }
-            disapproveDiv.append(disul)
-            disapprovalMemberList.append(disapproveDiv)
-            const divrow = document.createElement('div')
-            divrow.setAttribute('class', 'row')
-            const divcol1 = document.createElement('div')
-            divcol1.setAttribute('class', 'col-sm-6')
-            divcol1.append(approvalA)
-            divcol1.append(approvalcount)
-            divcol1.append (approvalMemberList)
-            divrow.append(divcol1)
-            const divcol2 = document.createElement('div')
-            divcol2.setAttribute('class', 'col-sm-6')
-            divcol2.append(disapprovalA)
-            divcol2.append(disapprovalcount)
-            divcol2.append(disapprovalMemberList)
-            divrow.append(divcol2)
-            cinners[i].append(h1)
-            cinners[i].append(h4)
-            cinners[i].append(divrow)
-			cinners[i].append(h6)
+            
 			//NEW CODE (FEEDBACK STUFF)
-			const hr = document.createElement('hr')
-            cinners[i].append(hr)
-			const addFeedbackBtn = document.createElement('button')
-            addFeedbackBtn.innerText = "Add Feedback"
-            addFeedbackBtn.setAttribute('onclick', `showFeedbackAddForm(${i})`)
-            addFeedbackBtn.style.cssFloat = 'left'
-            addFeedbackBtn.style.margin = '10px'
-            addFeedbackBtn.style.marginLeft = '180px'
-            addFeedbackBtn.setAttribute('class', 'btn')
-            addFeedbackBtn.setAttribute('class', 'btn-secondary')
-            cinners[i].append(addFeedbackBtn)
-            const chooseAltBtn = document.createElement('button')
-            chooseAltBtn.innerText = 'Choose This Alt'
-            chooseAltBtn.setAttribute('onclick', `pickAlternative(${i})`)
-            chooseAltBtn.style.cssFloat = 'right'
-            chooseAltBtn.style.margin = '10px'
-            chooseAltBtn.style.marginRight = '180px'
-            chooseAltBtn.setAttribute('class', 'btn')
-            chooseAltBtn.setAttribute('class', 'btn-secondary')
-            cinners[i].append(chooseAltBtn) 
-            const feedbackFormDiv = document.createElement('div')
-            feedbackFormDiv.setAttribute('class', `feedbackForm`)
-            cinners[i].append(feedbackFormDiv)
-            const hr2 = document.createElement('hr')
-            cinners[i].append(hr2)
 
 			if(aFeedbacks[i]!=null) {
-				
-			
-	            const feedLabel = document.createElement('h2')
-	            feedLabel.innerText='FEEDBACK:'
-	            cinners[i].append(feedLabel)
-	            const divOfFeedbacks = document.createElement('div')
-	            divOfFeedbacks.setAttribute('class', 'collapse')
-	            divOfFeedbacks.setAttribute('id', `feedbackcollapse${i}`)
-	            const showFeedbacks = document.createElement('button')
-	            showFeedbacks.setAttribute('class', 'btn')
-	            showFeedbacks.setAttribute('class', 'btn-primary')
-	            showFeedbacks.setAttribute('type', 'button')
-	            showFeedbacks.setAttribute('data-toggle', 'collapse')
-	            showFeedbacks.setAttribute('data-target', `#feedbackcollapse${i}`)
-	            showFeedbacks.innerText = 'Show Feedbacks'
-	            cinners[i].append(showFeedbacks)
+	            const divOfFeedbacks = document.querySelectorAll('.feedbacks')[i]
 	            //TODO: add for loop to go through feedback for alternative, adding it.
 	            for(let x=0; x<aFeedbacks[i].length; x++) {
 	            	const desc = document.createElement('p')
@@ -291,11 +229,6 @@ function createCarousel(aNames, aDescrips, aIds, aApproves, aDisapproves, num, a
 	            cinners[i].append(divOfFeedbacks)
 			}
 
-
-            for(let k=0; k<5; k++) {
-            	const br = document.createElement('br')
-            	cinners[i].append(br)
-            }
 			
         }
     for(let i=num; i<5; i++) {
